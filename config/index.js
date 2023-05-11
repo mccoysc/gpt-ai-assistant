2217cdcf748ea5e57b7384c80af9d923d46d2521
mccoysc <364589886@qq.com>
2023-05-11 10:53:39 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ const config = Object.freeze({
   APP_ENV: env.NODE_ENV || 'production',
   APP_DEBUG: env.APP_DEBUG === 'true' || false,
   APP_URL: env.APP_URL || null,
-  APP_PORT: env.APP_PORT || env.PORT || null,
+  APP_PORT: env.PORT || env.APP_PORT || null,
   APP_LANG: env.APP_LANG || 'zh_TW',
   APP_WEBHOOK_PATH: env.APP_WEBHOOK_PATH || '/webhook',
   APP_API_TIMEOUT: env.APP_API_TIMEOUT || 9000,
